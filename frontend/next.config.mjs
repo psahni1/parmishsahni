40f39de8a5a5fc4f7b52d71d2c0958e2e59b8b2c
psahni1 <parmish.dev@outlook.com>
2025-10-07 15:58:44 +1100

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverActions: { bodySizeLimit: '20mb' } }
-}
-export default nextConfig
+  reactStrictMode: true,
+  experimental: {
+    serverActions: { bodySizeLimit: "20mb" },
+  },
+  images: { remotePatterns: [] },
+};
+export default nextConfig;
